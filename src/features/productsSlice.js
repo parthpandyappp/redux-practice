927e07477741cc25d07c298529ba22312694fd50
parthpandyappp <parthpandyappp@gmail.com>
2023-04-28 16:55:21 +0530

    fix: api content fetched over https

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -10,7 +10,7 @@ export const getAllProducts = createAsyncThunk("products/getAllProducts", async 
     try {
         const res = await axios({
             method: "GET",
-            url: "http://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68",
+            url: "https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68",
         })
         return res.data
     } catch (error) {
