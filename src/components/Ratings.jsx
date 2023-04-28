283b36f72a5dd9460b012efca31ae47092fc5fa9
parthpandyappp <parthpandyappp@gmail.com>
2023-04-28 17:26:58 +0530

    chores: changed star family in rating component from fs to bs

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaStar, FaRegStarHalf, FaRegStar } from "react-icons/fa";
+import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 
 const Rating = ({ rating }) => {
   const fullStars = Math.floor(rating);
@@ -8,15 +8,15 @@ const Rating = ({ rating }) => {
   const stars = [];
 
   for (let i = 0; i < fullStars; i++) {
-    stars.push(<FaStar className="text-yellow-600" key={i} />);
+    stars.push(<BsStarFill className="text-yellow-600" key={i} />);
   }
 
   if (hasHalfStar) {
-    stars.push(<FaRegStarHalf className="text-yellow-600" key={fullStars} />);
+    stars.push(<BsStarHalf className="text-yellow-600" key={fullStars} />);
   }
 
   for (let i = 0; i < emptyStars; i++) {
-    stars.push(<FaRegStar key={fullStars + (hasHalfStar ? 1 : 0) + i} />);
+    stars.push(<BsStar key={fullStars + (hasHalfStar ? 1 : 0) + i} />);
   }
 
   return (
